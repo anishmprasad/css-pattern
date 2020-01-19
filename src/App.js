6ab112aa1e3962224174ba5ceaae4e5148116ce2
Anish.M.Prasad <anish.m.prasad@gmail.com>
2020-01-19 19:06:42 +0530

    css update

--- src/App.js
+++ src/App.js
@@ -3,24 +3,74 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div id='wrapper'>
+			<div>
+				<div className='container'>
+					<div class='animate' id='cell-1-1-1'></div>
+					<div class='animate' id='cell-1-2-1'></div>
+					<div class='animate' id='cell-2-1-1'></div>
+					<div class='animate' id='cell-2-2-1'></div>
+					<div class='animate' id='cell-3-1-1'></div>
+					<div class='animate' id='cell-3-2-1'></div>
+					<div class='animate' id='cell-4-1-1'></div>
+					<div class='animate' id='cell-4-2-1'></div>
+					<div class='animate' id='cell-5-1-1'></div>
+					<div class='animate' id='cell-5-2-1'></div>
+					<div class='animate' id='cell-6-1-1'></div>
+					<div class='animate' id='cell-6-2-1'></div>
+					<div class='animate' id='cell-7-1-1'></div>
+					<div class='animate' id='cell-7-2-1'></div>
+					<div class='animate' id='cell-8-1-1'></div>
+					<div class='animate' id='cell-8-2-1'></div>
+					<div class='animate' id='cell-9-1-1'></div>
+					<div class='animate' id='cell-9-2-1'></div>
+					<div class='animate' id='cell-10-1-1'></div>
+					<div class='animate' id='cell-10-2-1'></div>
+					<div class='animate' id='cell-11-1-1'></div>
+					<div class='animate' id='cell-11-2-1'></div>
+					<div class='animate' id='cell-12-1-1'></div>
+					<div class='animate' id='cell-12-2-1'></div>
+					<div class='animate' id='cell-13-1-1'></div>
+					<div class='animate' id='cell-13-2-1'></div>
+					<div class='animate' id='cell-14-1-1'></div>
+					<div class='animate' id='cell-14-2-1'></div>
+					<div class='animate' id='cell-15-1-1'></div>
+					<div class='animate' id='cell-15-2-1'></div>
+					<div class='animate' id='cell-16-1-1'></div>
+					<div class='animate' id='cell-16-2-1'></div>
+					<div class='animate' id='cell-17-1-1'></div>
+					<div class='animate' id='cell-17-2-1'></div>
+					<div class='animate' id='cell-18-1-1'></div>
+					<div class='animate' id='cell-18-2-1'></div>
+					<div class='animate' id='cell-19-1-1'></div>
+					<div class='animate' id='cell-19-2-1'></div>
+					<div class='animate' id='cell-20-1-1'></div>
+					<div class='animate' id='cell-20-2-1'></div>
+					<div class='animate' id='cell-21-1-1'></div>
+					<div class='animate' id='cell-21-2-1'></div>
+					<div class='animate' id='cell-22-1-1'></div>
+					<div class='animate' id='cell-22-2-1'></div>
+					<div class='animate' id='cell-23-1-1'></div>
+					<div class='animate' id='cell-23-2-1'></div>
+					<div class='animate' id='cell-24-1-1'></div>
+					<div class='animate' id='cell-24-2-1'></div>
+					<div class='animate' id='cell-25-1-1'></div>
+					<div class='animate' id='cell-25-2-1'></div>
+					<div class='animate' id='cell-26-1-1'></div>
+					<div class='animate' id='cell-26-2-1'></div>
+					<div class='animate' id='cell-27-1-1'></div>
+					<div class='animate' id='cell-27-2-1'></div>
+					<div class='animate' id='cell-28-1-1'></div>
+					<div class='animate' id='cell-28-2-1'></div>
+					<div class='animate' id='cell-29-1-1'></div>
+					<div class='animate' id='cell-29-2-1'></div>
+					<div class='animate' id='cell-30-1-1'></div>
+					<div class='animate' id='cell-30-2-1'></div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
